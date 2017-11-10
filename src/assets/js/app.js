3f67b485758af5ca26b51178b739154ff750aae2
Melvin Law <melvin.law@bizcube.com.sg>
2017-11-10 13:22:07 +0800

    Copied app.js from login branch

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
+import Cookies from 'js-cookie';
 import whatInput from 'what-input';
 
 window.$ = $;
+window.Cookies = Cookies;
 
 import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
@@ -10,10 +12,75 @@ import Foundation from 'foundation-sites';
 
 $(document).foundation();
 
+var pageName = "";
+var appCookie;
 //document ready
 $(function(){
-  
+  //get page name
+  pageName = getPageName();
+
+
+
+  $('.items').on('click', '.add', function () {
+      var imageId = $(this).data("id");
+      list.add(JSON.stringify(imageId));
+      var exists = list.exists(JSON.stringify(imageId))
+  });
+
+  //set login cookie
+  if (typeof Cookies.getJSON('appCookie') === 'undefined') {
+    Cookies.set('appCookie', {
+    },
+    { expires: 1 });
+  }
+  else {
+    appCookie = Cookies.getJSON('appCookie');
+  }
+
+  if (!appCookie.username && pageName.toLowerCase() != 'login') {
+
+    var pageURL = window.location;
+    Cookies.set('appCookie', {
+      redirectPage: (pageURL != '') ? pageURL : 'index.html'
+    });
+    window.location.href = 'login.html';
+  }
+
+  $('#logOut').click(function() {
+    $.ajax({
+      url: "https://portal.taksys.com.sg/Support/Sec1.Logout.json",
+      method: "POST",
+      dataType: "json",
+      xhrFields: {
+        withCredentials: true
+      },
+      data: {
+        'data': {},
+        'WebPartKey':'021cb7cca70748ff89795e3ad544d5eb',
+        'ReqGUID': 'b4bbedbf-e591-4b7a-ad20-101f8f656277'
+      }
+    })
+    .done(function(data) {
+      console.log( "Logout success" );
+      if (typeof Cookies.getJSON('appCookie') !== 'undefined')
+        Cookies.remove('appCookie');
+      window.location.href = 'login.html';
+    })
+    .fail(function( jqXHR, textStatus ) {
+      console.log( "Logout fail" );
+      console.log(jqXHR);
+      console.log( "Request failed: " + textStatus );
+    });
+
+    return false;
+  });
 });
+
+function getPageName() {
+  var pageName = $('body').attr('id').replace('page-','');
+  return pageName;
+
+}
 
 //convert date to dd/mm/yyyy
 function convertDate(inputFormat) {
